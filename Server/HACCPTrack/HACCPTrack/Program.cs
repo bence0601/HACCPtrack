6f3db1e0da74b8f8f0cb7b535f54fdfdc3a9141d
Bence Nagy <nagybence0601@gmail.com>
2024-06-12 18:26:15 +0200

    Commit : JWT token authorization added, it should work correctly, but it needs further testing

--- Server/HACCPTrack/HACCPTrack/Program.cs
+++ Server/HACCPTrack/HACCPTrack/Program.cs
@@ -20,8 +20,7 @@ AddIdentity();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();     
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ITokenService, TokenService>();
 ConfigureServices();
