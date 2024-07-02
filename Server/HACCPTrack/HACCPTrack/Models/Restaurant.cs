2023a6fb4f73decb2df0d60997ba6149e7cc480d
Gulyas Mate <mategulyas01@gmail.com>
2024-07-02 14:00:57 +0200

    Log and CheckItem entities created, restaurant and user are refactored.

--- Server/HACCPTrack/HACCPTrack/Models/Restaurant.cs
+++ Server/HACCPTrack/HACCPTrack/Models/Restaurant.cs
@@ -9,9 +9,12 @@
         public int CreatedById { get; set; }
         public User CreatedBy { get; set; }
         public ICollection<User> Users { get; set; }
+        public ICollection<Log> Logs { get; set; }
         public Restaurant()
         {
             Id = Guid.NewGuid().ToString();
+            Users = new List<User>();
+            Logs = new List<Log>();
         }
     }
 }
--- Server/HACCPTrack/HACCPTrack/Models/User.cs
+++ Server/HACCPTrack/HACCPTrack/Models/User.cs
@@ -15,9 +15,13 @@ namespace HACCPTrack.Models
         [JsonIgnore]
         public IdentityRole IdentityRole { get; set; }
 
+        public string RestaurantId { get; set; }
+        public Restaurant Restaurant { get; set; }
+        public ICollection<Log> Logs { get; set; }
         public User()
         {
             Id = Guid.NewGuid().ToString();
+            Logs = new List<Log>();
         }
     }
 }
