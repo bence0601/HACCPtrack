5b0d63427f5dad625cfac90ffbb19e76a15104b4
Gulyas Mate <mategulyas01@gmail.com>
2024-07-02 18:05:40 +0200

    checkStatus is communicate with the backend.

--- Client/HACCPTrackFrontend/components/CheckServerStatus/CheckServerStatus.js
+++ Client/HACCPTrackFrontend/components/CheckServerStatus/CheckServerStatus.js
@@ -1,10 +1,31 @@
 import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import { ActivityIndicator } from "react-native-paper";
+import API_BASE_URL from "../../config";
 
 export default function CheckServerStatus() {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const checkStatus = async () => {
+      try {
+        const response = await fetch(`${API_BASE_URL}/status`);
+        if (response.ok) {
+          const data = await response.json();
+          if (data.status === "ok") {
+            console.log("Server is now listening. (first fetch is working)");
+            setLoading(false);
+          }
+        } else {
+          console.error("Server returned an error:", response.status);
+        }
+      } catch (error) {
+        console.error("Error checking server status:", error);
+      }
+    };
+
+    checkStatus();
+  }, []);
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       {loading ? (
