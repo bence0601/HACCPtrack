ddef667fb5154866513311883eec261344573585
Gulyas Mate <mategulyas01@gmail.com>
2024-07-27 01:02:28 +0200

    HomeScreen small edit.

--- Client/HACCPTrackFrontend/screens/Home/HomeScreen.js
+++ Client/HACCPTrackFrontend/screens/Home/HomeScreen.js
@@ -1,14 +1,15 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import React from "react";
 import { useAuth } from "../../Auth/AuthContext";
 
-export default function HomeScreen() {
-  const { email } = useAuth();
+export default function HomeScreen({ navigate }) {
+  const { email, logout } = useAuth();
 
   return (
     <View>
       <Text>HomeScreen</Text>
       {email ? <Text>hello {email}!</Text> : <Text>Login</Text>}
+      <Button title="Logout" onPress={() => logout()} />
     </View>
   );
 }
