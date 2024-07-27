20b64fe8be6b0883f1aa49e59e2f1d8e8d009980
Bence Nagy <nagybence0601@gmail.com>
2024-07-27 16:34:55 +0200

    Commit : Added CRUD operations to RestaurantService class

--- Server/HACCPTrack/HACCPTrack/Services/RestaurantServices/IRestaurantService.cs
+++ Server/HACCPTrack/HACCPTrack/Services/RestaurantServices/IRestaurantService.cs
@@ -7,5 +7,8 @@ namespace HACCPTrack.Services.RestaurantServices
     {
         public Task<List<Restaurant>> GetAllRestaurantsAsync();
         public Task<Restaurant> CreateRestaurantAsync(RestaurantDTO restaurant);
+        public Task<List<Restaurant>> DeleteRestaurantByIdAsync(string id);
+        public Task<Restaurant> GetRestaurantByIdAsync(string id);
+        public Task<Restaurant> UpdateRestaurantAsync(string id, RestaurantDTO updatedRestaurant);
     }
 }
